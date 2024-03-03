f3f2b9204e8102a073222a938c25fab126f1773b
BingerBing <129381171+BingerBing@users.noreply.github.com>
2024-03-03 16:01:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wx23497257e9a2f201"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "240b2e43d0a7e0235587629ddf177828"
 # 模板消息id
 # 每日消息
-template_id1 = "wX-************************************"
+template_id1 = "	_A-eJBSQqT9k_sqieW-XFDFy7b_sVABLZLc4Mg4BCjs"
 # 课程消息,上课提醒
 template_id2 = "loe1yHWr************************************"
 # 晚安心语
 template_id3 = "-fun9-2************************************"
 # 接收公众号消息的微信号
 # 这是openid
-user = ["ojhdR50T******************"]
+user = ["wxid_f94te1qc983b12"]
 
 # 信息配置
 # 所在省份
